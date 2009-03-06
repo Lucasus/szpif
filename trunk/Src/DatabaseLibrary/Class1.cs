d05a74ca061bb0da1977150b4a784aa93378f40b
lukasz.wiatrak <lukasz.wiatrak@ddd455a0-0a54-11de-b321-f573fdaff03e>
2009-03-06 20:17:50 +0000

    Dodany interfejs Repository oraz prosty przykładowy test jednostkowy w NUnit

--- trunk/Src/DatabaseLibrary/Class1.cs
+++ trunk/Src/DatabaseLibrary/Class1.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BusinessLogic;
 
 namespace DatabaseLibrary
 {
-    public class Class1
+    public interface IEmployeeRepository
     {
+        void Add(Employee employee);
+        void Update(Employee employee);
+        void Remove(Employee employee);
+        Employee GetById(Guid productId);
+        Employee GetByName(string name);
+        ICollection<Employee> GetByCategory(string category);
     }
 }
