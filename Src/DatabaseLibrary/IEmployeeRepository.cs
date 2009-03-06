404eec5c7aa545df6a67432b6ddad99716f0da9d
lukasz.wiatrak <lukasz.wiatrak@ddd455a0-0a54-11de-b321-f573fdaff03e>
2009-03-06 20:18:52 +0000

    Poprawiona nazwa pliku

---
+++ Src/DatabaseLibrary/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BusinessLogic;
+
+namespace DatabaseLibrary
+{
+    public interface IEmployeeRepository
+    {
+        void Add(Employee employee);
+        void Update(Employee employee);
+        void Remove(Employee employee);
+        Employee GetById(Guid productId);
+        Employee GetByName(string name);
+        ICollection<Employee> GetByCategory(string category);
+    }
+}
