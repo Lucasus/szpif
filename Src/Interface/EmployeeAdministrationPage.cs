459fa717c207db389608b1c19ce28ae9d90b68b2
lukasz.wiatrak <lukasz.wiatrak@ddd455a0-0a54-11de-b321-f573fdaff03e>
2009-03-11 20:01:17 +0000

    DataGrid w widoku administrowania pracownikami odczytuje z bazy dane :) :) :)

--- Src/Interface/EmployeeAdministrationPage.cs
+++ Src/Interface/EmployeeAdministrationPage.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DatabaseLibrary;
 
 namespace Interface
 {
@@ -13,9 +14,22 @@ namespace Interface
     [ToolboxBitmap(typeof(TabPage))]
     public partial class EmployeeAdministrationPage : TabPage
     {
+        private SzpifDatabase database;
         public EmployeeAdministrationPage(string text) : base(text)
         {
+            database = new SzpifDatabase();
             InitializeComponent();
+            DataTable Employees = database.getEmployeesAdministrationView();
+            // Turn this off so column names do not come from data source
+            EmployeesGridView.AutoGenerateColumns = false;
+            EmployeesGridView.DataSource = Employees;
+            // Specify table as data source
+           // EmployeesGridView.DataMember = "EmployeeAdministrationView";  // Table in dataset
+            // Tie the columns in the grid to column names in the data table
+            EmployeesGridView.Columns[0].DataPropertyName = "Id";
+            EmployeesGridView.Columns[1].DataPropertyName = "Login";
+            EmployeesGridView.Columns[2].DataPropertyName = "Name";
+            EmployeesGridView.Columns[3].DataPropertyName = "Uprawnienia";
         }
     }
 }
