53ffb817dacbeeb2d59dbcb7639566265774cea5
lukasz.wiatrak <lukasz.wiatrak@ddd455a0-0a54-11de-b321-f573fdaff03e>
2009-03-12 15:46:30 +0000

    Trochę zrefaktoryzowana całość - part II

---
+++ trunk/Src/BusinessLogic/Context.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLogic
+{
+    public class Context
+    {
+        private string currentUserLogin;
+        private string currentUserPassword;
+        private ICollection<string> currentPermissions;
+
+        public Context(string login, string password, ICollection<string> perm)
+        {
+            this.currentUserLogin = login;
+            this.currentUserPassword = password;
+            this.currentPermissions = perm;
+        }
+
+        public string CurrentUserLogin
+        {
+            get { return currentUserLogin; }
+            set { currentUserLogin = value; }
+        }
+
+        public string CurrentUserPassword
+        {
+            get { return currentUserPassword; }
+            set { currentUserPassword = value; }
+        }
+
+        public ICollection<string> CurrentPermissions
+        {
+            get { return currentPermissions; }
+            set { currentPermissions = value; }
+        }
+
+
+    }
+}
