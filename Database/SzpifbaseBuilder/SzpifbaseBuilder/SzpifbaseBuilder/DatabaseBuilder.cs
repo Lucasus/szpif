825db68b74409e9dd47226ccf10266c7fdcbf6d4
lukasz.wiatrak <lukasz.wiatrak@ddd455a0-0a54-11de-b321-f573fdaff03e>
2009-04-10 19:43:12 +0000

    Dodałem wymagane biblioteki

---
+++ Database/SzpifbaseBuilder/SzpifbaseBuilder/SzpifbaseBuilder/DatabaseBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DatabaseLibrary;
+using System.Data.SqlClient;
+
+namespace SzpifbaseBuilder
+{
+    public class DatabaseBuilder
+    {
+        private void InitializeConnection()
+        {
+            SqlConnection connection = (SqlConnection)SzpifDatabase.getEmptyConnection();
+            connection.ConnectionString =
+               "Data Source=localhost\\SQLEXPRESS;"
+             + "Initial Catalog=szpifDatabase;"
+             + "Integrated Security=SSPI;";
+            SzpifDatabase.Connection = connection;
+        }
+    }
+}
