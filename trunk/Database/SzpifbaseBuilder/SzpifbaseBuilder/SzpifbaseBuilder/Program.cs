04867fe798ca417ce62000c6eb797824b59de9b5
lukasz.wiatrak <lukasz.wiatrak@ddd455a0-0a54-11de-b321-f573fdaff03e>
2009-04-10 19:43:12 +0000

    Dodałem wymagane biblioteki

--- trunk/Database/SzpifbaseBuilder/SzpifbaseBuilder/SzpifbaseBuilder/Program.cs
+++ trunk/Database/SzpifbaseBuilder/SzpifbaseBuilder/SzpifbaseBuilder/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DatabaseLibrary;
+using System.Data.SqlClient;
 
 namespace SzpifbaseBuilder
 {
@@ -10,6 +11,9 @@ namespace SzpifbaseBuilder
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadKey();
         }
     }
 }
