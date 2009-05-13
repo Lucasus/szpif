629a65e8b88c91675306ab922bedae0ca1e96cb4
mk2543 <mk2543@ddd455a0-0a54-11de-b321-f573fdaff03e>
2009-05-13 06:01:07 +0000

    tabele do bazy danych: wszystko zwiazane z pracownikami, klientami, zadania, projekty, zlecenia, rozliczenia tygodniowe

---
+++ trunk/Database/src/TextFunctions/TextFunctions/RegularExpressions.cs
@@ -0,0 +1,16 @@
+﻿using  Microsoft.SqlServer.Server;
+using System.Text.RegularExpressions;
+
+
+// klasa zajmujaca sie sprawdzaniem czy jakis tekst pasuje do wyrazenia regularnego 
+// wywolywana jako procedura CLR w bazie danych
+public class RegularExpressions{
+
+    [Microsoft.SqlServer.Server.SqlFunction()]
+    public static bool RegExMatch(string Input, string Pattern) {
+        Regex RegexInstance = new Regex(Pattern);
+        return RegexInstance.IsMatch(Input);
+    }
+}
+
+ 
